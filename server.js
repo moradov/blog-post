f198aef9d5bec447a8d63ef5ca41f6d09b734a3c
moradov <50326856+moradov@users.noreply.github.com>
2020-06-23 15:50:21 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+
+// connect to database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// defind Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/posts", require("./routes/posts"));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("the server work at port " + PORT));
